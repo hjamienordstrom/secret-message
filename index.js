4197866ca8c9056e93747f0e3746b70c0abade4f
Hanna Nordstrom <hjamienordstrom@Hannas-MacBook-Pro.local>
2021-10-25 02:18:15 -0400

    complete app

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+//destructuring hash
+const { hash } = window.location;
+const message = atob(hash.replace('#',''))
+
+if(message){
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#message-show').classList.remove('hide');
+
+    document.querySelector('h1').innerHTML = message;
+}
+//pulls text from input and outputs into console
 document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault();
 
-    const input = document.querySelector('input');
-    
-    console.log(input.value)
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide')
+
+    const input = document.querySelector('#message-input');
+
+    //encrypts into a string and prints out
+    const encrypted = btoa(input.value);
+    const linkInput = document.querySelector('#link-input');
+    linkInput.value = `${window.location}#${encrypted}`;
+    linkInput.select();
+
+    //console.log(input.value)
 })
+
+
+  
